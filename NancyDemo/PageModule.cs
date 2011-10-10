c1967aef2e21d86c987da2fecd7957fce5558c65
Chris Sainty <chris.sainty@quids.com.au>
2011-10-10 19:17:34 +1100

    Simple hello world to test Nancy on AppHarbor

---
+++ NancyDemo/PageModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyDemo
+{
+	public class PageModule : NancyModule
+	{
+		public PageModule()
+		{
+			Get["/"] = p => View["Default"];
+		}
+	}
+}
