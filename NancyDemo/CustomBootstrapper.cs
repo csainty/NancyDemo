9e4858fb43d24d979642c9f38fe5953359f10c2a
Chris Sainty <csainty@hotmail.com>
2012-04-22 11:06:02 +1000

    Updated to Nancy v0.10.0

--- NancyDemo/CustomBootstrapper.cs
+++ NancyDemo/CustomBootstrapper.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Nancy.Bootstrappers.Ninject;
+﻿using Nancy;
 using NancyDemo.Data;
-using Ninject;
 
 namespace NancyDemo
 {
-	public class CustomBootstrapper : NinjectNancyBootstrapper
+	public class CustomBootstrapper : DefaultNancyBootstrapper
 	{
-		protected override void InitialiseInternal(IKernel container)
-		{
-			base.InitialiseInternal(container);
-			container.Bind<IDataStore>().To<InMemoryDataStore>().InSingletonScope();
-		}
+        protected override void ConfigureApplicationContainer(TinyIoC.TinyIoCContainer container)
+        {
+            container.Register<IDataStore, InMemoryDataStore>().AsSingleton();
+        }
 	}
 }
